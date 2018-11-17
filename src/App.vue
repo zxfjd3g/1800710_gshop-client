e0886c11db614debe3066231812f31d80fadf56b
zxfjd3g <258147149@qq.com>
2018-11-17 11:49:56 +0800

    init app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FootGuide/>
+  </div>
+</template>
+
+<script>
+  import FootGuide from './components/FootGuide/FootGuide.vue'
+
+  export default {
+    components: {
+      FootGuide
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    padding 10px
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+/*
+路由器对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+
+// 声明使用vue插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  // 应用中所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
