1c595a4ffff793115b3d91836dd2d1a70b365760
zxfjd3g <258147149@qq.com>
2018-11-19 09:52:42 +0800

    ajax

---
+++ src/api/index.js
@@ -0,0 +1,14 @@
+/*
+包含n个接口请求函数的模块
+函数的返回值是promise对象
+ */
+import ajax from './ajax'
+
+// [1、根据经纬度获取位置详情](#1根据经纬度获取位置详情)<br/>
+export const reqAddress = (longitude, latitude) => ajax(`/position/${latitude},${longitude}`)
+
+// [2、获取食品分类列表](#2获取食品分类列表)<br/>
+export const reqFoodCategorys = () => ajax('/index_category')
+
+// [3、根据经纬度获取商铺列表](#3根据经纬度获取商铺列表)<br/>
+export const reqShops = ({longitude, latitude}) => ajax('/shops', {latitude, longitude})
